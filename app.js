9a518e827f923907feb6620cf7da7a770706e0ee
DbsYT <86447116+DbsYT@users.noreply.github.com>
2023-12-05 20:44:52 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+$(document).ready(function(){
+  var hqforks = 0;
+  var hqmoney = 0;
+  var hqforksPlus = -1;
+  var hqautoPlasticPlus = 0;
+  var hqautoForkerPrice=100;
+  var hqPlasticPrice = 1;
+  var menu;
+  $("#forks").click(function(){
+    hqforks += hqforksPlus;
+    changeinventory();
+    changemarket();
+  });
 
+  function changeinventory(){
+    $("#hqmoney").html("Money: $0", hqmoney);
+    if(hqforks == 1){
+      $("#hqforks").html("You now own " + hqForks + "fork.");
+    }else{
+       $("#hqforks").html("You now own " + hqForks + "forks.");
+    }
+  }
+});
