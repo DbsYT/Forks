ddb1c0db010c6c4fb46d960b6e784e87cc0e12ce
DbsYT <86447116+DbsYT@users.noreply.github.com>
2023-12-05 20:53:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   var hqforks = 0;
   var hqmoney = 0;
   var hqforksPlus = -1;
-  var hqautoPlasticPlus = 0;
+  var hqautoForkerPlus = 0;
   var hqautoForkerPrice=100;
-  var hqPlasticPrice = 1;
+  var hqforkPrice = 1;
   var menu;
   $("#forks").click(function(){
     hqforks += hqforksPlus;
@@ -13,7 +13,7 @@ $(document).ready(function(){
   });
 
   function changeinventory(){
-    $("#hqmoney").html("Money: $0", hqmoney);
+    $("#hqmoney").html("Money: $", hqmoney);
     if(hqforks == 1){
       $("#hqforks").html("You now own " + hqforks + "fork.");
     }else{
