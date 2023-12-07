825d7f0201ef82d246b84bce991dde4228f22ff2
DbsYT <86447116+DbsYT@users.noreply.github.com>
2023-12-07 06:21:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,72 @@
 $(document).ready(function(){
-    var hqforks = 0;
-    var hqmoney = 0;
-    var hqforksPlus = 1;
-    var hqautoForkerPlus = 0;
-    var hqautoForkerPrice= 100;
-    var hqforkPrice = 1;
+    var logs = 0;
+    var stone = 0;
+    var pickaxes = 0;
+    var money = 0;
+    var logPlus = 1;
+    var stonePlus = 1;
+    var autoLogPlus = 0;
+    var autoChopperPrice = 100;
+    var pickaxePrice = 50;
+    var logPrice = 1;
     var menu;
 
-    $("#forks").click(function(){
-      hqforks += hqforksPlus;
-      changeinventory();
-      changemarket();
+    setInterval(function(){
+        logs += autoLogPlus;
+        changeInventory();
+        changeMarket();
+    }, 1000);
+
+    $("#chop").click(function(){
+        logs += logPlus;
+        changeInventory();
+        changeMarket();
     });
+
+    $("#mineStone").click(function(){
+        if(pickaxes == 0){
+            alert("You have nothing to mine stone with!");
+        }else{
+            stone += stonePlus;
+            changeInventory();
+        }
+    });
+
+    $("#sell1").click(function(){
+        logs--;
+        money += logPrice;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#sell10").click(function(){
+        logs-=10;
+        money += logPrice * 10;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#sellAll").click(function(){
+        money += logPrice * logs;
+        logs = 0;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#autoChopper").click(function(){
+        money -= autoChopperPrice;
+        autoLogPlus++;
+        changeInventory();
+        changeMarket();
+    });
+
+    $("#buyPickaxe").click(function(){
+        money -= pickaxePrice;
+        pickaxes++;
+        changeInventory();
+        changeMarket();
+    });
+
 
     $("#visit").click(function(){
         menu = switchMenu("marketplace");
@@ -19,40 +74,61 @@ $(document).ready(function(){
     });
 
     $("#return").click(function(){
-       menu = switchMenu("main");
+        menu = switchMenu("main");
     });
-  
-  function changeinventory(){
-      $("#tmoney").html("Money: $", hqmoney);
-      if(hqforks == 1){
-        $("#tforks").html("You now own " + hqforks + " fork.");
-      }else{
-         $("#tforks").html("You now own " + hqforks + " forks.");
-      }
-  }
 
-    function changemarket(){
-        if(hqforks > 0){
+
+    function changeInventory(){
+        $("#money").html("Money: $" + money);
+
+        if(logs == 1){
+            $("#logs").html("You now own " + logs + " log.");
+        }else{
+            $("#logs").html("You now own " + logs + " logs.");
+        }
+
+        if(stone > 0){
+            $("#stone").html("You now own " + stone + " piece(s) of stone.");
+        }else{
+            $("#stone").html("");
+        }
+
+        if(pickaxes > 0){
+            $("#pickaxes").html("You now own " + pickaxes + " pickaxe(s).");
+        }else{
+            $("#pickaxes").html("");
+        }
+    }
+
+    function changeMarket(){
+        if(logs > 0){
             $("#sellAll").css("display", "block");
         }else{
             $("#sellAll").css("display", "none");
         }
-        if(hqforks >= 1){
+        if(logs >= 1){
             $("#sell1").css("display", "block");
         }else{
             $("#sell1").css("display", "none");
         }
-        if(hqforks >= 10){
+        if(logs >= 10){
             $("#sell10").css("display", "block");
         }else{
             $("#sell10").css("display", "none");
         }
-         if(hqforks >= 100){
-            $("#sell100").css("display", "block");
+
+        if(money >= autoChopperPrice){
+            $("#autoChopper").css("display", "block");
         }else{
-            $("#sell100").css("display", "none");
+            $("#autoChopper").css("display", "none");
         }
-  }
+
+        if(money >= pickaxePrice){
+            $("#buyPickaxe").css("display", "block");
+        }else{
+            $("#buyPickaxe").css("display", "none");
+        }
+    }
 
     function switchMenu(menu){
         $(".menus").children().css("display", "none");
