c5ede192997eb5decfadca9b60c5db4905ba122c
DbsYT <86447116+DbsYT@users.noreply.github.com>
2023-12-06 20:28:25 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,63 @@
 $(document).ready(function(){
-  var hqforks = 0;
-  var hqmoney = 0;
-  var hqforksPlus = -1;
-  var hqautoForkerPlus = 0;
-  var hqautoForkerPrice=100;
-  var hqforkPrice = 1;
-  var menu;
-  $("#forks").click(function(){
-    hqforks += hqforksPlus;
-    changeinventory();
-    changemarket();
-  });
+    var hqforks = 0;
+    var hqmoney = 0;
+    var hqforksPlus = 1;
+    var hqautoForkerPlus = 0;
+    var hqautoForkerPrice= 100;
+    var hqforkPrice = 1;
+    var menu;
 
+    $("#forks").click(function(){
+      hqforks += hqforksPlus;
+      changeinventory();
+      changemarket();
+    });
+
+    $("#visit").click(function(){
+      menu = switchmenu("marketplace");
+      changemarket();
+    });
+
+    $("#return").click(function(){
+       menu = switchmenu("main");
+    });
+  
   function changeinventory(){
-    $("#hqmoney").html("Money: $", hqmoney;
-    if(forks == 1){
-      $("#hqforks").html("You now own " + hqforks + "fork.");
-    }else{
-       $("#hqforks").html("You now own " + hqforks + "forks.");
-    }
+      $("#tmoney").html("Money: $", hqmoney);
+      if(hqforks == 1){
+        $("#tforks").html("You now own " + hqforks + " fork.");
+      }else{
+         $("#tforks").html("You now own " + hqforks + " forks.");
+      }
   }
+
+  function changemarket(){
+    if(hqforks > 0){
+    $("#sellAll").css("display", "block");
+    }else{
+      $("#sellAll").css("display", "none");
+    }
+    if(hqforks >= 1){
+      $("#sell1").css("display", "block");
+    }else{
+      $("#sell1").css("display", "none");
+    }
+    if(hqforks >= 10){
+      $("#sell10").css("display", "block");
+    }else{
+      $("#sell10").css("display", "none");
+    }
+    if(hqforks > 100){
+      $("#sell100").css("display", "block");
+    }else{
+      $("#sell100").css("display", "none");
+    }
+
+  }
+
+  function switchmenu(menu){
+    $(".menus").children().css("display", "none");
+    $("." + menu).css("display", "block");
+    return menu;
+}
 });
