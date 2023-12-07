4f5954eba4e3cba97980c969c170c38ac62f9196
DbsYT <86447116+DbsYT@users.noreply.github.com>
2023-12-06 20:57:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,9 +55,9 @@ $(document).ready(function(){
 
   }
 
-  function switchmenu(menu){
-    $(".menus").children().css("display", "none");
-    $("." + menu).css("display", "block");
-    return menu;
-}
+    function switchMenu(menu){
+        $(".menus").children().css("display", "none");
+        $("." + menu).css("display", "block");
+        return menu;
+    }
 });
